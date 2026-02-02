e25b4221d5d10561f7b411fffedd03eb88dc588a
Jennelyn R. Flordeliz <20227738@s.ubaguio.edu>
2026-02-02 07:22:16 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jenflordeliz.github.io/FG__LAB2_Flordeliz/',
   plugins: [react()],
 })
